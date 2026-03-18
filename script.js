cb0aaa28c0444c5dc6b7c9f49de33ce2d14ffb56
Renata Gusmão <renatagusmaoads@gmail.com>
2026-03-18 09:06:08 -0300

    feat: implementação da interface e consumo de API de clima
    Adiciona os arquivos principais da aplicação (index.html, style.css e script.js).
    
    Implementada interface responsiva para busca de clima por cidade e integração com APIs públicas para obtenção das coordenadas geográficas e dos dados meteorológicos. O sistema permite que o usuário informe uma cidade e visualize informações climáticas atualizadas na interface.

---
+++ script.js
@@ -0,0 +1,25 @@
+function buscarClima(){
+
+    const url = "https://api.open-meteo.com/v1/forecast?latitude=-8.05&longitude=-34.9&current_weather=true";
+
+    fetch(url)
+    .then(response => response.json())
+    .then(dados => {
+
+        const clima = dados.current_weather;
+
+        document.getElementById("nomeCidade").innerText = "Recife";
+
+        document.getElementById("temperatura").innerText = clima.temperature + " °C";
+
+        document.getElementById("descricao").innerText = "Velocidade do vento: " + clima.windspeed + " km/h";
+
+        document.getElementById("umidade").innerText = "Direção do vento: " + clima.winddirection;
+
+    })
+    .catch(error => {
+        alert("Erro ao buscar dados da API");
+        console.log(error);
+    });
+
+}
